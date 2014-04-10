e426359c685ea6c5079d170f926cce8c61c7388d
Ben Kiefer <kieferfam@msn.com>
2014-04-10 04:09:19 +0100

    matcher and additional test coverage

--- src/main/java/com/sai/controller/UnprotectedController.java
+++ src/main/java/com/sai/controller/UnprotectedController.java
@@ -11,14 +11,14 @@ import org.springframework.web.servlet.view.RedirectView;
 @Controller
 public class UnprotectedController {
     @RequestMapping(value = "/unprotected", method = RequestMethod.GET)
-    public ModelAndView protectedGetUrl() {
+    public ModelAndView unprotectedGetUrl() {
         ModelAndView modelAndView = new ModelAndView("unprotected");
         modelAndView.addObject("nameRequest", new NameRequest());
         return modelAndView;
     }
 
     @RequestMapping(value = "/unprotected", method = RequestMethod.POST)
-    public ModelAndView protectedPostUrl(NameRequest nameRequest, RedirectAttributes redirectAttributes) {
+    public ModelAndView unprotectedPostUrl(NameRequest nameRequest, RedirectAttributes redirectAttributes) {
         String message = String.format("Your name is: %s %s", nameRequest.getFirst(), nameRequest.getLast());
         redirectAttributes.addFlashAttribute("message", message);
         return new ModelAndView(new RedirectView("/unprotected", true, true, false));
