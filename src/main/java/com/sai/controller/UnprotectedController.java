78aba573dec152436fe496bb5568ff9df0a9484a
Ben Kiefer <kieferfam@msn.com>
2014-04-10 03:47:29 +0100

    simple spring security app

---
+++ src/main/java/com/sai/controller/UnprotectedController.java
@@ -0,0 +1,26 @@
+package com.sai.controller;
+
+import com.sai.forms.NameRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class UnprotectedController {
+    @RequestMapping(value = "/unprotected", method = RequestMethod.GET)
+    public ModelAndView protectedGetUrl() {
+        ModelAndView modelAndView = new ModelAndView("unprotected");
+        modelAndView.addObject("nameRequest", new NameRequest());
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/unprotected", method = RequestMethod.POST)
+    public ModelAndView protectedPostUrl(NameRequest nameRequest, RedirectAttributes redirectAttributes) {
+        String message = String.format("Your name is: %s %s", nameRequest.getFirst(), nameRequest.getLast());
+        redirectAttributes.addFlashAttribute("message", message);
+        return new ModelAndView(new RedirectView("/unprotected", true, true, false));
+    }
+}
